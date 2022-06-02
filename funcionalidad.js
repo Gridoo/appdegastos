87683b5093e9bc3514f8084c1d38b389b72a3e2d
Guido <guido.bassi@hotmail.com>
2022-06-02 17:44:47 -0300

    Añadiendo funcionalidad

---
+++ funcionalidad.js
@@ -0,0 +1,22 @@
+const nombres=[];
+const pagos=[];
+
+function tocar() {
+    let x= document.getElementById("nombre").value;
+    let y= document.getElementById("pago").value;
+    nombres.push=(x);
+    pagos.push=(y);
+    mostrar(x);
+}
+
+function mostrar (x) {
+  var newDiv = document.createElement("div");
+  var newContent = document.createTextNode(x);
+  newDiv.appendChild(newContent);
+
+  var currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+}
+
+console.log(nombres);
+console.log(pagos);
